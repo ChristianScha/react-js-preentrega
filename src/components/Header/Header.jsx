c8fb4f72087893cd905cb0b45b1c36572fc77f63
Christian Schahab <christian@MacBookPro.fibertel.com.ar>
2026-05-27 20:49:13 -0300

    first changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
+import logo from "../../assets/react.svg";
+
 export const Header = () => {
   return (
     <header>
-      <div className="logo-container"></div>
+      <div className="logo-container">
+        <Link>
+          <img src={logo} />
+          <span>Ractiva</span>
+        </Link>
+      </div>
     </header>
   );
 };
