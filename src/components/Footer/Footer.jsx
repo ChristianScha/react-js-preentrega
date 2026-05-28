027ccf6bd60048552b25c507bb3ab8145e80f1a2
Christian Schahab <christian@MacBookPro.fibertel.com.ar>
2026-05-28 00:24:10 -0300

    avance del proyecto

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Sitio En Desarrollo por Churro Fan</p>
+      <nav>
+        <ul className="nav-list">
+          <li>WhatsApp</li>
+          <li>Instagram</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 import logo from "../../assets/react.svg";
+import "./Header.css";
+import { Nav } from "../Nav/Nav";
 
 export const Header = () => {
   return (
     <header>
       <div className="logo-container">
-        <Link>
+        <Link to={"/"}>
           <img src={logo} />
-          <span>Ractiva</span>
+          <span>Reactiva</span>
         </Link>
       </div>
+      <Nav />
     </header>
   );
 };
